e88edd2dc79207d8498314eeb035500ebedb6c33
SiddharthKasherwal <sk1251995@gmail.com>
2023-11-18 20:35:13 +0530

    New Files

--- app/(home)/loading.jsx
+++
@@ -1,9 +0,0 @@
-import { ImSpinner10 } from 'react-icons/im';
-
-export default function Loading() {
-  return (
-    <div className='flex items-center justify-center h-screen'>
-      <ImSpinner10 className='animate-spin w-8 h-8' />
-    </div>
-  );
-}
