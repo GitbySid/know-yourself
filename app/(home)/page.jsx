e61ce2b1b1af954086924ad876fb8573df011bdb
GitbySid <sk1251995@gmail.com>
2023-11-22 14:54:56 +0530

    feat: add login and skip buttons in homepage

--- app/(home)/page.jsx
+++ app/(home)/page.jsx
@@ -3,13 +3,19 @@
 import { useRouter } from 'next/navigation';
 import { Raleway, League_Spartan } from 'next/font/google';
 import { twMerge } from 'tailwind-merge';
+import { FiLogIn, FiSkipForward } from 'react-icons/fi';
 
 const raleway = Raleway({ subsets: ['latin'] });
 const leagueSpartan = League_Spartan({ subsets: ['latin'] });
 
 export default function HomePage() {
   const router = useRouter();
-  const handleClick = () => {
+
+  const login = () => {
+    // TODO: Add login button functionality
+  };
+
+  const skip = () => {
     return router.push('/ask');
   };
 
@@ -17,13 +23,13 @@ export default function HomePage() {
     <main className='flex flex-col items-center w-full h-screen'>
       <p
         className={twMerge(
-          'flex h-1/2 justify-center items-center text-secondary w-10 text-7xl sm:text-8xl break-normal text-center font-bold',
+          'flex h-[40%] justify-center items-center text-secondary w-10 text-7xl sm:text-8xl break-normal text-center font-bold',
           leagueSpartan.className
         )}
       >
         Know yourself
       </p>
-      <div className='flex flex-col h-1/2 justify-center items-center'>
+      <div className='flex flex-col h-[40%] justify-center items-center'>
         <p
           className={twMerge(
             'text-4xl sm:text-5xl p-5 max-w-xs sm:max-w-lg text-center leading-[50px] sm:leading-[60px]',
@@ -32,14 +38,27 @@ export default function HomePage() {
         >
           Discover your best version with
         </p>
-        <button
+        <p
           className={twMerge(
             'text-4xl sm:text-5xl max-w-xs sm:max-lg text-center leading-[50px] sm:leading-[60px] underline',
             raleway.className
           )}
-          onClick={handleClick}
         >
           Know Yourself
+        </p>
+      </div>
+      <div className='flex h-[20%] gap-5 p-5'>
+        <button
+          className='flex gap-2 items-center justify-center border border-white rounded-md p-3 hover:bg-white hover:text-black transition-all w-[100px] h-[50px]'
+          onClick={login}
+        >
+          Login <FiLogIn />
+        </button>
+        <button
+          className='flex gap-2 items-center justify-center border border-white rounded-md p-3 hover:bg-white hover:text-black transition-all w-[100px] h-[50px]'
+          onClick={skip}
+        >
+          Skip <FiSkipForward />
         </button>
       </div>
     </main>
