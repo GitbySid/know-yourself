e7b0fee7bc94076f88ceb9b8f566981be7f232d0
GitbySid <sk1251995@gmail.com>
2023-11-23 15:54:02 +0530

    feat: change search to home in navbar
    - Changed the search button to home button in navigation bar.
    - Added redirection to homepage on button click.

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,33 +1,35 @@
-import { FaSearch } from 'react-icons/fa';
+import Link from 'next/link';
+import { FaHome } from 'react-icons/fa';
 import { MdHistory } from 'react-icons/md';
 import { FaHeart } from 'react-icons/fa';
 import { RiAccountBoxFill } from 'react-icons/ri';
 import { IoIosSettings } from 'react-icons/io';
 
 export default function Navbar() {
-  // TODO: Add navigate functionality
+  // TODO: Change hrefs for navbar links
+
   return (
     <div className='flex fixed bottom-0 w-full items-center justify-between bg-[#02051c] h-20 p-5 sm:px-24'>
-      <div className='flex flex-col items-center'>
-        <FaSearch className='h-6 w-6 my-2' />
-        <p>Search</p>
-      </div>
-      <div className='flex flex-col items-center'>
+      <Link className='flex flex-col items-center' href='/'>
+        <FaHome className='h-6 w-6 my-2' />
+        <p>Home</p>
+      </Link>
+      <Link className='flex flex-col items-center' href='/'>
         <MdHistory className='h-6 w-6 my-2' />
         <p>History</p>
-      </div>
-      <div className='flex flex-col items-center'>
+      </Link>
+      <Link className='flex flex-col items-center' href='/'>
         <FaHeart className='h-6 w-6 my-2' />
         <p>Like</p>
-      </div>
-      <div className='flex flex-col items-center'>
+      </Link>
+      <Link className='flex flex-col items-center' href='/'>
         <RiAccountBoxFill className='h-6 w-6 my-2' />
         <p>Settings</p>
-      </div>
-      <div className='flex flex-col items-center'>
+      </Link>
+      <Link className='flex flex-col items-center' href='/'>
         <IoIosSettings className='h-6 w-6 my-2' />
         <p>Account</p>
-      </div>
+      </Link>
     </div>
   );
 }
