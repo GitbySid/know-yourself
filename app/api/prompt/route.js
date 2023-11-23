fd7eb91fefe5a746355f39db04c93c4fa48aab9f
GitbySid <sk1251995@gmail.com>
2023-11-23 16:19:54 +0530

    fix: handle error for invalid predefined prompt

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -3,6 +3,16 @@ import { getPredefinedPrompt } from './data';
 
 export async function POST(req) {
   const { label } = await req.json();
+
+  if (!label) {
+    return new NextResponse('Invalid Label', { status: 400 });
+  }
+
   const prompt = getPredefinedPrompt(label);
+
+  if (!prompt) {
+    return new NextResponse('Prompt Not Found', { status: 404 });
+  }
+
   return NextResponse.json(prompt);
 }
