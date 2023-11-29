019d73f4876b08c4b3e9f73a61f53d1d839b7001
GitbySid <sk1251995@gmail.com>
2023-11-29 17:12:29 +0530

    feat: fetch prompt form data from database

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,7 +1,14 @@
 import { NextResponse } from 'next/server';
+import dbConnect from '@/lib/dbConnect';
+
 import { getPredefinedPrompt } from './data';
+import InputQuestion from '@/models/InputQuestion';
+import ChoiceQuestion from '@/models/ChoiceQuestion';
+import PromptForm from '@/models/PromptForm';
 
 export async function GET(request) {
+  await dbConnect();
+
   const searchParams = request.nextUrl.searchParams;
   const promptId = searchParams.get('promptId');
 
@@ -9,11 +16,13 @@ export async function GET(request) {
     return new NextResponse('Invalid Prompt Id', { status: 400 });
   }
 
-  const prompt = getPredefinedPrompt(promptId);
+  const promptForm = await PromptForm.findOne({ promptId }).populate(
+    'question1 question2 question3 question4 question5'
+  );
 
-  if (!prompt) {
+  if (!promptForm) {
     return new NextResponse('Prompt Not Found', { status: 404 });
   }
 
-  return NextResponse.json(prompt);
+  return NextResponse.json(promptForm);
 }
--- scripts/seed.js
+++ scripts/seed.js
@@ -17,7 +17,56 @@ mongoose
 
 async function importData() {
   try {
-    // Insert data here
+    const question1 = new ChoiceQuestion({
+      question: 'Answer Level',
+      choices: ['Broad', 'Medium', 'Specific'],
+    });
+    await question1.save();
+
+    const question2 = new ChoiceQuestion({
+      question: 'How are you feeling right now?',
+      choices: [
+        'A) Excited and Energetic',
+        'B) Melancholic or Thoughtful',
+        'C) Relaxed or Chill',
+        'D) Curious and Adventurous',
+      ],
+    });
+    await question2.save();
+
+    const question3 = new ChoiceQuestion({
+      question: 'What genre are you in the mood for?',
+      choices: [
+        'A) Action/Adventure',
+        'B) Drama',
+        'C) Comedy',
+        'D) Mystery/Thriller',
+      ],
+    });
+    await question3.save();
+
+    const question4 = new InputQuestion({
+      question: 'What was the last movie you enjoyed and why did you like it?',
+    });
+    await question4.save();
+
+    const question5 = new InputQuestion({
+      question:
+        "Is there a specific theme you're interested in tonight ( friendship,space, historical events, etc.)?",
+    });
+    await question5.save();
+
+    const promptForm = new PromptForm({
+      promptId: '1',
+      label: 'What movie to watch?',
+      question1: question1.id,
+      question2: question2.id,
+      question3: question3.id,
+      question4: question4.id,
+      question5: question5.id,
+    });
+    await promptForm.save();
+
     console.log('Data added successfully.');
     process.exit(0);
   } catch (error) {
