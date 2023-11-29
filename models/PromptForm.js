ed0ecb9f5bf3664cc9c2663010bdf68557584acd
GitbySid <sk1251995@gmail.com>
2023-11-29 16:23:35 +0530

    fix: change incorrect model for PromptForm

--- models/PromptForm.js
+++ models/PromptForm.js
@@ -2,6 +2,11 @@ import mongoose from 'mongoose';
 
 const PromptFormSchema = new mongoose.Schema(
   {
+    promptId: {
+      type: String,
+      required: [true, 'PromptId is required.'],
+      unique: true,
+    },
     label: {
       type: String,
       required: [true, 'Label is required.'],
@@ -9,8 +14,9 @@ const PromptFormSchema = new mongoose.Schema(
     },
     question1: { type: mongoose.Schema.Types.ObjectId, ref: 'ChoiceQuestion' },
     question2: { type: mongoose.Schema.Types.ObjectId, ref: 'ChoiceQuestion' },
-    question3: { type: mongoose.Schema.Types.ObjectId, ref: 'InputQuestion' },
+    question3: { type: mongoose.Schema.Types.ObjectId, ref: 'ChoiceQuestion' },
     question4: { type: mongoose.Schema.Types.ObjectId, ref: 'InputQuestion' },
+    question5: { type: mongoose.Schema.Types.ObjectId, ref: 'InputQuestion' },
   },
   {
     timestamps: true,
